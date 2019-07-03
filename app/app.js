6f2353e1bf7aeca3798f7edeb6d4e4f5d63693da
flozender <tayeeb.hasan@gmail.com>
2019-07-03 16:26:31 +0530

    updated

--- app/app.js
+++ app/app.js
@@ -13,5 +13,27 @@ app.get('/marketplace', (req,res) => {
     res.render('marketplace');
 });
 
+app.post('/profile', (req,res) => {
+    res.render('profile');
+});
+
+app.get('/user', (req,res) => {
+    res.render('user');
+});
+app.get('/admin', (req,res) => {
+    res.render('admin');
+});
+app.get('/register', (req,res) => {
+    res.render('register');
+});
+app.get('/manage', (req,res) => {
+    res.render('manage');
+});
+
+
+
+
+
+
 app.listen(3030);
 console.log("listening on port 3030");
